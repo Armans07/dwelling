9c75272a80b4534010b815638815542202b7f3d3
Armans07 <sikder89098@gmail.com>
2023-10-10 01:16:09 +0600

    added new

--- src/fb/fb.config.js
+++ src/fb/fb.config.js
@@ -5,13 +5,21 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: `${process.env.NEXT_PUBLIC_apiKey}`,
+//   authDomain: `${process.env.NEXT_PUBLIC_authDomain}`,
+//   projectId: `${process.env.NEXT_PUBLIC_projectId}`,
+//   storageBucket: `${process.env.NEXT_PUBLIC_storageBucket}`,
+//   messagingSenderId: `${process.env.NEXT_PUBLIC_messagingSenderId}`,
+//   appId: `${process.env.NEXT_PUBLIC_appId}`,
+// };
 const firebaseConfig = {
-  apiKey: `${process.env.NEXT_PUBLIC_apiKey}`,
-  authDomain: `${process.env.NEXT_PUBLIC_authDomain}`,
-  projectId: `${process.env.NEXT_PUBLIC_projectId}`,
-  storageBucket: `${process.env.NEXT_PUBLIC_storageBucket}`,
-  messagingSenderId: `${process.env.NEXT_PUBLIC_messagingSenderId}`,
-  appId: `${process.env.NEXT_PUBLIC_appId}`,
+  apiKey: "AIzaSyA0hQ_n6NGid8uwJZetl8S59jn-o83tkV8",
+  authDomain: "dwelling-e835a.firebaseapp.com",
+  projectId: "dwelling-e835a",
+  storageBucket: "dwelling-e835a.appspot.com",
+  messagingSenderId: "655521408532",
+  appId: "1:655521408532:web:76d89517255a84c02ca901"
 };
 
 console.log(firebaseConfig);
